183e5b57a2e0b605136fe3182e80dce5a3fb89fd
sanandkumar <Anandkumar.Gandigudi@datavail.com>
2015-06-10 14:01:01 +0530

    Added New Project Simple Console New App

---
+++ SimpleConsoleNewApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleConsoleNewApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hellow, Its a New Simple Console Application");
+        }
+    }
+}
